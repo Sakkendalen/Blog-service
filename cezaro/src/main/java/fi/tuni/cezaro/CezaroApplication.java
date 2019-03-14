ff68bf1be7c98a2e8494fd7fc712055e1308d458
Saku Tynjälä <sakutynjala@gmail.com>
2019-03-14 11:18:18 +0200

    maven project Spring init
    -create mvn project spring initializer

---
+++ cezaro/src/main/java/fi/tuni/cezaro/CezaroApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.cezaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CezaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CezaroApplication.class, args);
+	}
+
+}
