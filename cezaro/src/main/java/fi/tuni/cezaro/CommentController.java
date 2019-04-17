0c1fecf619b16c492bb3b1e8ca521ee0398330b6
Saku Tynjälä <sakutynjala@gmail.com>
2019-04-17 13:31:16 +0300

    Add class CommentController
    Class CommentController
    Class Comment
    Interface CommentRepository
    basic methods, constructor

--- cezaro/src/main/java/fi/tuni/cezaro/CommentController.java
+++ cezaro/src/main/java/fi/tuni/cezaro/CommentController.java
@@ -1,4 +1,34 @@
 package fi.tuni.cezaro;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/comment")
 public class CommentController {
+
+    @Autowired
+    CommentRepository comRepo;
+
+    @GetMapping(value = "/get")
+    public Iterable<Comment> findAll(){
+        return comRepo.findAll();
+    }
+
+    @RequestMapping(value = "/asdasd/{postID}", method= RequestMethod.GET)
+    public Iterable<Comment> findCommentInPost(@PathVariable long postID){
+        return comRepo.findByPostIdOrderByDatetimeAsc(postID);
+    }
+
+    @RequestMapping(value = "/add")
+    public void addComment(@RequestBody Comment comment){
+        comRepo.save(new Comment(comment.getPseudonym(), comment.getContent(), comment.getPostId(), LocalDateTime.now().withNano(0)));
+    }
+
+    @RequestMapping(value = "/delete/{id}")
+    public void delComment(@PathVariable long id){
+        comRepo.deleteById(id);
+    }
 }
--- cezaro/src/main/java/fi/tuni/cezaro/CommentRepository.java
+++ cezaro/src/main/java/fi/tuni/cezaro/CommentRepository.java
@@ -2,5 +2,8 @@ package fi.tuni.cezaro;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+    List<Comment> findByPostIdOrderByDatetimeAsc(long postID);
 }
