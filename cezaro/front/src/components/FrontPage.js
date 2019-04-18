a639df3f45c3c098b9852047a8b5112ab0860018
Saku Tynjälä <sakutynjala@gmail.com>
2019-04-18 15:28:35 +0300

    Modify Stylesheet

--- cezaro/front/src/components/FrontPage.js
+++ cezaro/front/src/components/FrontPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+require("../styles/FrontPage.css");
+
 class FrontPage extends Component{
 
     render() {
         return (
-            <div>
+            <div className="frondpagediv">
                 <h1>Cezaro Blog Front page</h1>
                 <h1>Cezaro Blog Front page</h1>
                 <h1>Cezaro Blog Front page</h1>
