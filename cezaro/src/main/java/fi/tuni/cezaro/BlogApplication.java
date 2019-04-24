159b5de41f9997fdc75065b7d7a2bdc2da1b1b5f
Saku Tynjälä <sakutynjala@gmail.com>
2019-04-24 11:42:36 +0300

    Update Class BlogApplication.java
    Print Curl Commands.

--- cezaro/src/main/java/fi/tuni/cezaro/BlogApplication.java
+++ cezaro/src/main/java/fi/tuni/cezaro/BlogApplication.java
@@ -8,26 +8,27 @@ public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
-		//PostController
-		//curl -X GET http://localhost:8080/api/posts	->	OK
-		//curl -X GET http://localhost:8080/api/post/{id}	-> OK
-		//curl -X GET http://localhost:8080/api/search/{variable}	-> OK
-		//curl -X GET http://localhost:8080/api/prevpost/{id}	-> OK
-		//curl -X GET http://localhost:8080/api/nextpost/{id}	-> OK
-		//curl -X POST http://localhost:8080/api/delete/{id} 	-> OK
-		//curl -X POST -H "Content-Type: application/json" -d "{\"author\": \"String\", \"title\": \"String\", \"content\":\"TEXT\"}" http://localhost:8080/api/add		->OK
-		//curl -X POST -H "Content-Type: application/json" -d "{\"author\": \"String\", \"title\": \"String\", \"content\":\"TEXT\"}" http://localhost:8080/api/update/{id}		->OK
+		System.out.println("PostController CURL:");
+		System.out.println("curl -X GET http://localhost:8080/api/posts");
+		System.out.println("curl -X GET http://localhost:8080/api/post/{id}");
+		System.out.println("curl -X GET http://localhost:8080/api/search/{variable}");
+		System.out.println("curl -X GET http://localhost:8080/api/prevpost/{id}");
+		System.out.println("curl -X GET http://localhost:8080/api/nextpost/{id}");
+		System.out.println("curl -X POST http://localhost:8080/api/delete/{id} ");
+		System.out.println("curl -X POST -H \"Content-Type: application/json\" -d \"{\\\"author\\\": \\\"String\\\", \\\"title\\\": \\\"String\\\", \\\"content\\\":\\\"TEXT\\\"}\" http://localhost:8080/api/add");
+		System.out.println("curl -X POST -H \"Content-Type: application/json\" -d \"{\\\"author\\\": \\\"String\\\", \\\"title\\\": \\\"String\\\", \\\"content\\\":\\\"TEXT\\\"}\" http://localhost:8080/api/update/{id}");
 
-		//CommentController
-		//curl -X GET http://localhost:8080/comment/get		-> OK
-		//curl -X GET http://localhost:8080/comment/getComments/{postID}	->OK
-		//curl -X POST -H "Content-Type: application/json" -d "{\"pseudonym\": \"String\", \"content\": \"String\", \"postId\":\"long\"}" http://localhost:8080/comment/add		-> OK
-		//curl -X POST http://localhost:8080/comment/delete/{commentID}	-> OK
-		//curl -X POST http://localhost:8080/comment/like/[commentID}	-> OK
+		System.out.println("CommentController CURL:");
+		System.out.println("curl -X GET http://localhost:8080/comment/get");
+		System.out.println("curl -X GET http://localhost:8080/comment/getComments/{postID}");
+		System.out.println("curl -X POST -H \"Content-Type: application/json\" -d \"{\\\"pseudonym\\\": \\\"String\\\", \\\"content\\\": \\\"String\\\", \\\"postId\\\":\\\"long\\\"}\" http://localhost:8080/comment/add\t");
+		System.out.println("curl -X POST http://localhost:8080/comment/delete/{commentID}");
+		System.out.println("curl -X POST http://localhost:8080/comment/like/[commentID}");
 
-		//UserController
-		//curl -X POST -H "Content-Type: application/json" -d "{\"userName\": \"Saku\", \"password\": \"admin2\"}" http://localhost:8080/login	-> OK
-		//curl -X POST -H "Content-Type: application/json" -d "{\"userName\": \"Saku\", \"password\": \"admin2\"}" http://localhost:8080/addUser	-> OK Throws ConstraintViolationException
+
+		System.out.println("UserController CURL:");
+		System.out.println("curl -X POST -H \"Content-Type: application/json\" -d \"{\\\"userName\\\": \\\"Saku\\\", \\\"password\\\": \\\"admin2\\\"}\" http://localhost:8080/login");
+		System.out.println("curl -X POST -H \"Content-Type: application/json\" -d \"{\\\"userName\\\": \\\"Saku\\\", \\\"password\\\": \\\"admin2\\\"}\" http://localhost:8080/addUser");
 
 	}
 
